0665db8b7d70295b238e13c4993bf8e6276e4bac
bridgetcarr2 <122331284+bridgetcarr2@users.noreply.github.com>
2023-07-30 21:32:21 -0400

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+    <link rel="stylesheet" href="style.contact">
+</head>
+<body>
+    <style>
+        
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+/* this is a background color to the top navigation*/
+        .topnav {
+            overflow: hidden;
+            background-color: #95A7A7;
+        }
+/*This styles the links inside the navigation bar */
+        .topnav a {
+            float: right;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 30px 15px;
+            text-decoration: none;
+            font-size: 17px;
+
+        }
+/*This changes the color of the links on hover */
+        .topnav a:hover {
+            background-color: #f4C6B8;
+            color: black;
+        }
+/*this adds a color to the active/ current link */
+        .topnav a.active {
+            background-color: #e68164;
+             color: white;
+}
+</style>
+</head>
+<body>
+<!--this is my navigation bar-->
+ <div class="topnav">
+    <a class="active" href="">Contact</a>
+    <a href = "">About</a>
+    <a href = "">Plant Gallery</a>
+    <a href = "">Home</a>
+    <img src="logo.png" width="150" height="150">
+         <a style="color:white; background:#333; margin-right: 20px; font-size: 24px;">First Lady Floral</a> 
+</div>
+</body>
+<!--This is the map-->
+
+<body>
+    <div class="container">
+        <h1>Our Location</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.647860797259!2d-111.73102838498245!3d33.38025828079399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872baf1d9d267d85%3A0x68a4a4daf32f56a8!2s4824%20E%20Baseline%20Rd%20STE%20113%2C%20Mesa%2C%20AZ%2085206!5e0!3m2!1sen!2sus!4v1680014784734!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+
+<!--this positions the text to the right of the image-->
+
+     <body>
+        <div style = "position: relative; left:625px; top: -625px;">
+            <h3> Phone:</h3>
+            <p> call: 183-850-3848 or fax:  480-8923-237923</p>
+        </div>
+     </body>
+
+     <body>
+        <div style = "position: relative; left:625px; top: -600px;">
+            <h3> Address:</h3>
+            <p> 1253 E. Rose Rd Suite 303, Mesa, AZ 8274936</p>
+        </div>
+     </body>
+
+     <body>
+        <div style = "position: relative; left:625px; top: -575px;">
+            <h3> Hours</h3>
+            <p> Mon-FRI: 8:00am-4:30pm
+                <br>
+                Sat-Sun: 8:00am-3pm
+            </p>
+        </div>
+    </body>
+
+           <!--Footer-->
+     <footer>
+     <p> Email: FirstLadyFLoral123@gmail.com </p>
+      <p>1253 E. Rose Rd Suite 303, Mesa, AZ 8274936</p>
+      <p> Phone: 183-850-3848</p>
+      <p> Fax: 480-8923-237923</p>
+     
+      </footer>
+
+        <!--Footer Style-->
+      <style>
+        footer {
+          text-align: center;
+          padding: 3px;
+          background-color:#e68164;
+          color: white;
+        }
+        </style>
+    
+</body>
+</body>
+</html>
